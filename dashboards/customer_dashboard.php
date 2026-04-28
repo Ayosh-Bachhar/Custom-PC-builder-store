7b9f8c8521abb725d1f9cefb28da0cba28570e8f
Ayosh-Bachhar <ayoshbachhar@gmail.com>
2026-04-28 23:19:43 +0600

    Config, login, logout, register, dashboard.

---
+++ dashboards/customer_dashboard.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["User_ID"]) || $_SESSION["Role"] != "Customer") {
+    header("Location: ../index.php?role=login");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Customer Dashboard</title>
+</head>
+<body>
+
+    <h1>Customer Dashboard</h1>
+
+    <p>Welcome, <?php echo $_SESSION["Name"]; ?></p>
+
+    <a href="customer_profile.php">Manage Profile</a>
+    <br><br>
+
+    <a href="../logout.php">Logout</a>
+
+</body>
+</html>
